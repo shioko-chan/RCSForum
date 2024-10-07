3bb792226aa040fbcf9a3ac34b58421c03052fc5
Ayaya <3329058005@qq.com>
2024-10-07 18:24:17 +0800

    save

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 App({
   onLaunch: function () {
-    tt.requestAccess({
-      scopeList: [],
-      appid: "cli_a67536fc8d7c900c",
-      success(res) {
-        console.log(JSON.stringify(res));
-      },
-      fail(res) {
-        console.log(`requestAccess fail: ${JSON.stringify(res)}`);
-      },
+    tt.login({
+      success: function () {
+        tt.getUserInfo({
+          withCredentials: false,
+          success(res) {
+            console.log(JSON.stringify(res));
+          },
+          fail(res) {
+            console.log(`getUserInfo fail: ${JSON.stringify(res)}`);
+          }
+        });
+      }
     });
   }
 })
